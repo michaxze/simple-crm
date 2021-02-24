fff1b4222e46e08219bfa3a6a90087424d03cf7e
Mike Gimena <michaxze@gmail.com>
2021-02-23 21:54:52 -0400

    remove unused file

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,11 +1,5 @@
 class Api::ApiController < ActionController::API
-  #include Response
   include ExceptionHandler
   include ActionController::ImplicitRender
   include ActionView::Layouts
-
-
-  
-
-
 end
